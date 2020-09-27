6b765cd66aec186b60b1201965f2706bec51257d
Reinhard Michalke <reinhardmichalke@gmail.com>
2020-09-27 16:29:58 +0200

    bla

--- exercises/flask_container_ci/app/main_test.py
+++ exercises/flask_container_ci/app/main_test.py
@@ -1,2 +1,16 @@
 import requests
+import os
+import unittest
 
+url = os.getenv('APP_URL', 'http')
+
+
+class TestCase(unittest.TestCase):
+    def test_is_alive(self):
+        response = requests.get(url)
+        assert response.status_code == 200
+
+if __name__ == '__main__':
+    import xmlrunner
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='API-test-reports'))
+    unittest.main()
