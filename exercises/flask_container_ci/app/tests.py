2151ecf097b5af0bf4ca2f2d833b531106018619
Reinhard Michalke <reinhardmichalke@gmail.com>
2020-09-27 15:25:10 +0200

    added bug

--- exercises/flask_container_ci/app/tests.py
+++ exercises/flask_container_ci/app/tests.py
@@ -17,7 +17,7 @@ class TestCase(unittest.TestCase):
 
     def test_users_page(self):
         response = self.app.get('/users', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 300)
 
 
 if __name__ == '__main__':
