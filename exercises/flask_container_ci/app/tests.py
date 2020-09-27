71a490d077998aec4e78b2263970b9632c70c94a
Reinhard Michalke <reinhardmichalke@gmail.com>
2020-09-27 10:43:00 +0200

    added reportings

--- exercises/flask_container_ci/app/tests.py
+++ exercises/flask_container_ci/app/tests.py
@@ -21,4 +21,6 @@ class TestCase(unittest.TestCase):
 
 
 if __name__ == '__main__':
+    import xmlrunner
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
     unittest.main()
